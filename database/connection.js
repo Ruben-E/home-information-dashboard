52a1e048d840d688f177b45ab32c95d762df0034
Ruben Ernst <rubenernst@gmail.com>
2015-11-14 19:10:20 +0100

    Fixed wrong db

--- database/connection.js
+++ database/connection.js
@@ -2,6 +2,6 @@ var r = require('rethinkdb');
 
 module.exports = {
     getConnection: function () {
-        return r.connect({host: 'localhost', port: 28015, db: 'information-dashboard'});
+        return r.connect({host: 'localhost', port: 28015, db: 'test'});
     }
 };
