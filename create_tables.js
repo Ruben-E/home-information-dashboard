b246899f5513b3c897c327be3da79cb273f595dc
Ruben Ernst <rubenernst@gmail.com>
2015-11-14 18:17:28 +0100

    Added a test with a promise

--- create_tables.js
+++ create_tables.js
@@ -1,7 +1,10 @@
 var r = require('rethinkdbdash')();
 var Connection = require('./database/connection');
 
-var connection = Connection.getConnection();
+var connection = Connection.getConnection().then(function() {
+    console.log("test");
+});
+
 r.db('home-information').tableCreate('temperature').run(connection, function(err, result) {
     if (err) throw err;
 });
